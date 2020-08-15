0e81952d91635fd8b670e6aa1f9edfe7e59b29d0
Wanda <wmmcinturff@gmail.com>
2020-08-14 21:01:39 -0500

    revised expel function and fixed typo

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const buttonEvents = () => {
     document.getElementById('jumbotron-btn').addEventListener('click', buildForm);
     document.getElementById('form').addEventListener('click', addStudent);
     document.getElementById('form').addEventListener('click', buildCards);
-    document.querySelector('#student-buildCards').addEventListener('click', expelStudent);
+    document.querySelector('#student-cards').addEventListener("click", expelStudent);
+
 };
 
 const buildForm = () => {
@@ -83,7 +84,13 @@ const buildForm = () => {
       document.getElementById('inputStudent').value = '';
   }
   }};
+  const expelStudent = (e) => {
+    const expelButtonClicked = e.target.nodeName;
   
+    if (expelButtonClicked === 'A') {
+    studentCards.splice(expelButtonClicked, 1);
+    rebuildCards();
+  }};
   const rebuildCards = () => {
     let domString = '';
   for (let i = 0; i < studentCards.length; i++) {
@@ -98,12 +105,6 @@ const buildForm = () => {
     printToDom("student-cards", domString);
   };
   
-  const expelStudent = (e) => {
-    const expelButtonClicked = e.target.nodeName;
   
-    if (expelButtonClicked === 'A') {
-    studentCards.splice(expelButtonClicked, 1);
-    rebuildCards();
-  }};
   
   buttonEvents();
